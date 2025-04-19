79a4876e2a719399fc0ea74e17c87881d86cce6c
KaelanC-TechPriest <kae9car@gmail.com>
2025-04-19 11:17:02 -0600

    switch blink with nvim-cmp, oh god, please don't break

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -12,8 +12,8 @@ return {
 		-- Useful status updates for LSP.
 		{ "j-hui/fidget.nvim", opts = {} },
 
-		-- Allows extra capabilities provided by blink.cmp
-		"saghen/blink.cmp",
+		-- Allows extra capabilities provided by nvim-cmp
+		"hrsh7th/cmp-nvim-lsp",
 	},
 	config = function()
 		-- Brief aside: **What is LSP?**
@@ -186,7 +186,8 @@ return {
 		--  By default, Neovim doesn't support everything that is in the LSP specification.
 		--  When you add blink.cmp, luasnip, etc. Neovim now has *more* capabilities.
 		--  So, we create new capabilities with blink.cmp, and then broadcast that to the servers.
-		local capabilities = require("blink.cmp").get_lsp_capabilities()
+		local capabilities = vim.lsp.protocol.make_client_capabilities()
+		capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
 
 		-- Enable the following language servers
 		--  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
