266deaef899bd5f1f47ef8b79961aa07dcea5dee
KaelanC-TechPriest <kae9car@gmail.com>
2025-05-07 20:12:16 -0600

    all of neorg config & lazy-lock

--- .config/nvim/lua/plugins/neorg.lua
+++ .config/nvim/lua/plugins/neorg.lua
@@ -2,5 +2,28 @@ return {
 	"nvim-neorg/neorg",
 	lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
 	version = "*", -- Pin Neorg to the latest stable release
-	config = true,
+	config = function()
+		require("neorg").setup({
+			load = {
+				["core.defaults"] = {},
+				["core.concealer"] = {},
+				["core.dirman"] = {
+					config = {
+						workspaces = {
+							notes = "~/notes",
+							personal = "~/personal",
+							school = "~/school",
+						},
+						default_workspace = "notes",
+						vim.keymap.set("n", "<LocalLeader>nn", "<Plug>(neorg.dirman.new-note)"),
+					},
+				},
+				["core.summary"] = {},
+			},
+			vim.keymap.set("n", "<LocalLeader>nim", ":Neorg inject-metadata<CR>"),
+			vim.keymap.set("n", "<LocalLeader>num", ":Neorg update-metadata<CR>"),
+			vim.keymap.set("n", "<LocalLeader>ngs", ":Neorg generate-workspace-summary<CR>"),
+			vim.keymap.set("n", "<LocalLeader>nt", ":Neorg toc<CR>"),
+		})
+	end,
 }
