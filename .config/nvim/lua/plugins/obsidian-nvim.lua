ac060b6d9f4b3b237b647bf504280626bd0c37fa
KaelanC-TechPriest <der-tech-priest@proton.me>
2026-06-14 11:43:03 -0600

    feat(nvim): Add claudecode and obsidian plugins

--- .config/nvim/lua/plugins/obsidian-nvim.lua
+++ .config/nvim/lua/plugins/obsidian-nvim.lua
@@ -1,22 +1,29 @@
 return {
     "obsidian-nvim/obsidian.nvim",
     version = "*", -- use latest release, remove to use latest commit
+    lazy = true,
+    ft = "markdown",
     ---@module 'obsidian'
     ---@type obsidian.config
     opts = {
         legacy_commands = false, -- this will be removed in 4.0.0
         ui = {
-            enable = false
+            ignore_conceal_warn = true
         },
         workspaces = {
             {
                 name = "personal",
                 path = "~/personal/notes",
             },
-            {
-                name = "school",
-                path = "~/school",
-            },
         },
+    },
+    keys = {
+        { "<leader>ot", "<cmd>Obsidian tags<cr>", desc = "Obsidian Tags" },
+        { "<leader>on", "<cmd>Obsidian new<cr>", desc = "Obsidian New Note" },
+        { "<leader>om", "<cmd>Obsidian new-from-template<cr>", desc = "Obsidian New from Template" },
+        { "<leader>ob", "<cmd>Obsidian backlinks<cr>", desc = "Obsidian Backlinks" },
+        { "<leader>oo", "<cmd>Obsidian open<cr>", desc = "Obsidian Open" },
+        { "<leader>oc", "<cmd>Obsidian toc<cr>", desc = "Obsidian ToC" },
+        { "<leader>or", "<cmd>Obsidian rename<cr>", desc = "Obsidian Rename" },
     },
 }
